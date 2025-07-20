bd36972b0530aa3cec3a0523fa57bda9efc972d3
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-07-20 13:38:09 +0200

    feat: connect to MongoDB and pass db instance to route modules

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const mongoClient = require("mongodb").MongoClient;
 
 const employeesRoutes = require("./routes/employees.routes");
 const departmentsRoutes = require("./routes/departments.routes");
 const productsRoutes = require("./routes/products.routes");
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/api", employeesRoutes);
-app.use("/api", departmentsRoutes);
-app.use("/api", productsRoutes);
-
-app.use((req, res) => {
-  res.status(404).send({ message: "Not found..." });
-});
-
-app.listen("8000", () => {
-  console.log("Server is running on port: 8000");
-});
 
 mongoClient.connect(
   "mongodb://0.0.0.0:27017",
@@ -31,6 +14,25 @@ mongoClient.connect(
       console.log(err);
     } else {
       console.log("Successfully connected to the database");
+      const db = client.db("companyDB");
+
+      const app = express();
+      app.use(cors());
+      app.use(express.json());
+      app.use(express.urlencoded({ extended: false }));
+
+      // Przekazujemy bazę danych do routerów:
+      app.use("/api", employeesRoutes(db));
+      app.use("/api", departmentsRoutes(db));
+      app.use("/api", productsRoutes(db));
+
+      app.use((req, res) => {
+        res.status(404).send({ message: "Not found..." });
+      });
+
+      app.listen(8000, () => {
+        console.log("Server is running on port: 8000");
+      });
     }
   }
 );
