f31eb2984d8520b5fe90f43f07d4b1859cdb0a75
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-08-02 17:05:14 +0200

    Update Department controller

---
+++ controllers/departments.controller.js
@@ -0,0 +1,66 @@
+const Department = require("../models/department.model");
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Department.find());
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.getRandom = async (req, res) => {
+  try {
+    const count = await Department.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const dep = await Department.findOne().skip(rand);
+    if (!dep) res.status(404).json({ message: "Not found" });
+    else res.json(dep);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.getById = async (req, res) => {
+  try {
+    const dep = await Department.findById(req.params.id);
+    if (!dep) res.status(404).json({ message: "Not found" });
+    else res.json(dep);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.create = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const newDepartment = new Department({ name });
+    await newDepartment.save();
+    res.json({ message: "OK" });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    const updated = await Department.findByIdAndUpdate(
+      req.params.id,
+      { name: req.body.name },
+      { new: true }
+    );
+    if (updated) res.json(updated);
+    else res.status(404).json({ message: "Not found..." });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    const deleted = await Department.findByIdAndDelete(req.params.id);
+    if (deleted) res.json(deleted);
+    else res.status(404).json({ message: "Not found..." });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
