f7a533621549e5e1226c8f0f5c035d2359d378b1
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-08-02 17:40:09 +0200

    Add minlength and maxlength validation to Department model and test edge cases

--- models/test/department.test.js
+++ models/test/department.test.js
@@ -20,6 +20,17 @@ describe("Department Model", () => {
     }
   });
 
+  it('should throw an error if "name" is too short or too long', () => {
+    const cases = ["Abc", "abcd", "Lorem Ipsum, Lorem Ip"];
+    for (let name of cases) {
+      const dep = new Department({ name });
+
+      dep.validateSync((err) => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
   after(() => {
     mongoose.models = {};
   });
