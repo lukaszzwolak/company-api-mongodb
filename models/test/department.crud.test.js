868477891b5c5117e0501d19d99a9507c4b2ce96
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-08-02 18:30:27 +0200

    Add tests for reading Department data using find and findOne

--- models/test/department.crud.test.js
+++ models/test/department.crud.test.js
@@ -13,6 +13,28 @@ describe("Department", () => {
       console.error(err);
     }
   });
+
+  describe("Reading data", () => {
+    before(async () => {
+      const testDepOne = new Department({ name: "Department #1" });
+      await testDepOne.save();
+
+      const testDepTwo = new Department({ name: "Department #2" });
+      await testDepTwo.save();
+    });
+
+    it('should return all the data with "find" method', async () => {
+      const departments = await Department.find();
+      const expectedLength = 2;
+      expect(departments.length).to.equal(expectedLength);
+    });
+
+    it('should return the first department with "findOne" method', async () => {
+      const department = await Department.findOne({ name: "Department #1" });
+      expect(department.name).to.equal("Department #1");
+    });
+  });
+
   after(() => {
     mongoose.models = {};
   });
