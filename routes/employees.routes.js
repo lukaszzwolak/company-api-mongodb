169a491003ae7a0e9d10fa0e1d3a5410e85f76ff
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-08-02 17:06:47 +0200

    Update employee routes

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const EmployeeController = require("../controllers/employee.controller");
+const EmployeeController = require("../controllers/employees.controller");
 
-router.get("/employee", EmployeeController.getAll);
-router.get("/employee/random", EmployeeController.getRandom);
-router.get("/employee/:id", EmployeeController.getById);
-router.post("/employee", EmployeeController.create);
-router.put("/employee/:id", EmployeeController.update);
-router.delete("/employee/:id", EmployeeController.delete);
+router.get("/employees", EmployeeController.getAll);
+router.get("/employees/random", EmployeeController.getRandom);
+router.get("/employees/:id", EmployeeController.getById);
+router.post("/employees", EmployeeController.create);
+router.put("/employees/:id", EmployeeController.update);
+router.delete("/employees/:id", EmployeeController.delete);
 
 module.exports = router;
