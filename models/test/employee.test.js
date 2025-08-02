b1824ac2fa4701ca3d11cbc8384f2bd87fcc9803
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-08-02 17:57:29 +0200

    Add validation tests for Employee model schema

---
+++ models/test/employee.test.js
@@ -0,0 +1,45 @@
+const Employee = require("../employee.model");
+const expect = require("chai").expect;
+const mongoose = require("mongoose");
+
+describe("Employee Model", () => {
+  it('should throw an error if no "firstName", "lastName", "department"', () => {
+    const emp = new Employee({});
+
+    const err = emp.validateSync();
+    expect(err.errors.firstName).to.exist;
+    expect(err.errors.lastName).to.exist;
+    expect(err.errors.department).to.exist;
+  });
+
+  it('should throw an error if "firstName", "lastName", or "department" is not a string', () => {
+    const cases = [{}, []];
+
+    for (let value of cases) {
+      const emp = new Employee({
+        firstName: value,
+        lastName: value,
+        department: value,
+      });
+
+      const err = emp.validateSync();
+      expect(err.errors.firstName).to.exist;
+      expect(err.errors.lastName).to.exist;
+      expect(err.errors.department).to.exist;
+    }
+  });
+
+  it("should not thorw an error if all required fields are provided correctly", () => {
+    const emp = new Employee({
+      firstName: "John",
+      lastName: "Doe",
+      department: "HR",
+    });
+
+    const err = emp.validateSync();
+    expect(err).to.not.exist;
+  });
+  after(() => {
+    mongoose.models = {};
+  });
+});
