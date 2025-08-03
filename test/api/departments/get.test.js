2d7bf255f059f2397e8fd696e55cfd2eb256678f
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-08-03 13:44:39 +0200

    Add test data setup and teardown for GET /api/departments tests

--- test/api/departments/get.test.js
+++ test/api/departments/get.test.js
@@ -1,6 +1,7 @@
 const expect = require("chai").expect;
 const chaiHttp = require("chai-http");
 const server = require("../../server");
+const Department = require("../../models/department.model");
 
 chai.use(chaiHttp);
 
@@ -8,7 +9,24 @@ const expect = chai.expect;
 const request = chai.request;
 
 describe("GET /api/departments", () => {
+  before(async () => {
+    const testDepOne = new Department({
+      _id: "5d9f1140f10a81216cfd4408",
+      name: "Department #1",
+    });
+    await testDepOne.save();
+
+    const testDepTwo = new Department({
+      _id: "5d9f1159f81ce8d1ef2bee48",
+      name: "Department #2",
+    });
+    await testDepTwo.save();
+  });
   it("/ should return all departments", () => {});
   it("/:id should return one department by id", () => {});
   it("/random should return a random department", () => {});
+
+  after(async () => {
+    await Department.deleteMany();
+  });
 });
